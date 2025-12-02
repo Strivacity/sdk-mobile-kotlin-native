a31c11a96cb4b07769c006e014e083d0a7c6e3fc
Daniel VH <daniel.vargahali@strivacity.com>
2025-12-02 09:35:38 +0100

    feat(logging): Add logging interface to SDK

--- sdk/src/main/java/com/strivacity/android/native_sdk/Error.kt
+++ sdk/src/main/java/com/strivacity/android/native_sdk/Error.kt
@@ -1,6 +1,11 @@
 package com.strivacity.android.native_sdk
 
-sealed class Error : Throwable() {}
+/**
+ * Base class for all errors in the SDK.
+ */
+sealed class Error(message: String? = null, cause: Throwable? = null) : Throwable(message, cause) {
+  constructor(message: String) : this(message, null)
+}
 
 class OidcError(val error: String, val errorDescription: String?) : Error()
 
@@ -10,6 +15,6 @@ class UnknownError(val exception: Exception) : Error()
 
 class InvalidCallbackError(val reason: String) : Error()
 
-class HttpError(val statusCode: Int) : Error()
+class HttpError(val statusCode: Int) : Error(message = "HTTP Error with status code $statusCode") {}
 
 class SessionExpiredError : Error()
