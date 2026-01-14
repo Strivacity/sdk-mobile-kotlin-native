6c42871e64743f88f2597080fe6aa6be8228f0f3
Tamás Kriskó <tamas.krisko@strivacity.com>
2026-01-14 14:12:39 +0100

    sdk/...: add support for entry

--- sdk/src/main/java/com/strivacity/android/native_sdk/Error.kt
+++ sdk/src/main/java/com/strivacity/android/native_sdk/Error.kt
@@ -1,5 +1,8 @@
 package com.strivacity.android.native_sdk
 
+import java.util.Arrays
+import java.util.function.Predicate
+
 /**
  * Base class for all errors in the SDK.
  */
@@ -7,7 +10,7 @@ sealed class Error(message: String? = null, cause: Throwable? = null) : Throwabl
   constructor(message: String) : this(message, null)
 }
 
-class OidcError(val error: String, val errorDescription: String?) : Error()
+open class OidcError(val error: String, val errorDescription: String?) : Error()
 
 class HostedFlowCanceledError() : Error()
 
@@ -18,3 +21,27 @@ class InvalidCallbackError(val reason: String) : Error()
 class HttpError(val statusCode: Int) : Error(message = "HTTP Error with status code $statusCode") {}
 
 class SessionExpiredError : Error()
+
+class WorkflowError(error: String, errorDescription: String?) : OidcError(error, errorDescription) {
+
+  enum class WorkflowErrorId(private val id: String) {
+    MAGIC_LINK_EXPIRED("magicLinkExpired"),
+    CLIENT_MISMATCH("clientMismatch"),
+    INVALID_REDIRECT_URI("invalidRedirectUri");
+
+    companion object {
+      fun valueOfId(id: String): WorkflowErrorId? {
+        return Arrays.stream(entries.toTypedArray()).filter(byId(id)).findFirst().orElse(null)
+      }
+
+      /**
+       * Matcher used to filter stream by WorkflowError id
+       * @param idParam id to match against
+       * @return predicate that returns true if workflowError id matches, false otherwise
+       */
+      private fun byId(idParam: String): Predicate<WorkflowErrorId> {
+        return Predicate { workflowError: WorkflowErrorId -> workflowError.id == idParam }
+      }
+    }
+  }
+}
